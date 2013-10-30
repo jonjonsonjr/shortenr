0eaf7fdbfe5b94efd033905add0eadfc16a03326
Jon Johnson <jonjonsonjr@gmail.com>
2013-10-30 00:49:32 -0400

    Removed unused modules.

--- app.js
+++ app.js
@@ -2,22 +2,15 @@
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
-var dust = require('dustjs-linkedin');
-var cons = require('consolidate');
 var path = require('path');
 
 // all environments
 var app = express();
-app.engine('dust', cons.dust);
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'dustjs-linkedin');
-app.use(express.compress());
 app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 1000*60*60*24*14 }));
 
 // routes
 app.post('/', routes.create);
